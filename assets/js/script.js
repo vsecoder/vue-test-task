12beac4a1bc488fa04d10e964fd0d50e38c511c7
Vsevolod <kreepmeister@gmail.com>
2024-06-21 11:58:15 +0300

    BEMissimo?

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,6 @@
 window.onload = function() {
     let popup = window.document.querySelector('.popup');
-    let popup_open = window.document.querySelector('.popup__open');
+    let popup_open = window.document.querySelector('.cards__block-action');
     let popup_close = window.document.querySelector('.popup__close');
     classList = 'popup popup__overlay '
     
